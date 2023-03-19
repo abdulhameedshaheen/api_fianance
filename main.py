2f4221eb80f559fe7764700f62f718c74f66d2c7
abdulhameedshaheen <62672461+abdulhameedshaheen@users.noreply.github.com>
2023-03-19 08:24:37 +0300

    Send API news to Email

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import requests
+from send_email import send_email
+
 
 api_key = "439e692d12e842e1b5e840e98f2a971c"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2023-02-19&sortBy=publishedAt&apiKey" \
     "=439e692d12e842e1b5e840e98f2a971c"
 request = requests.get(url)
 content = request.json()
-
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
 
+    body = body + "Article:" + "\n" + "_______________" + "\n" + article["title"] + "\n" + \
+           "Description:" + "\n" + "________________" + "\n" + article["description"] + 4*"\n"
+
+
+send_email(body.encode('utf-8'))
